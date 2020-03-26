60c7c0a5adb4d5341f69a019da2b9c2d27031b92
dragomirandrei-webdev <dandrei.web@gmail.com>
2020-03-26 17:54:36 +0200

    more complex validation + redirect

--- assets/script/validation.js
+++ assets/script/validation.js
@@ -4,10 +4,31 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
+const loginEmail = document.getElementById("emailLogin");
+const loginPwd = document.getElementById("passwordLogin");
+
+let validLoginEmail = false;
+let validLoginPwd = false;
+
+let passw = /[0-9]/g;
+let validUsername = false;
+let validEmail = false;
+let validPwd = false;
+let validPwd2 = false;
+
+function sendToSuccesPage() {
+  if (validUsername && validEmail && validPwd && validPwd2) {
+    return window.location.replace("../assets/pages/success.html");
+  } else {
+    return;
+  }
+}
+
 form.addEventListener("submit", e => {
   e.preventDefault();
 
   checkInputs();
+  // checkLogin();
 });
 
 function checkInputs() {
@@ -19,8 +40,12 @@ function checkInputs() {
 
   if (usernameValue === "") {
     setErrorFor(username, "Username cannot be blank");
+  } else if (usernameValue.length < 5) {
+    setErrorFor(username, "Username should be longer than 4 characters.");
   } else {
     setSuccessFor(username);
+    validUsername = true;
+    // localStorage.setItem("username", usernameValue);
   }
 
   if (emailValue === "") {
@@ -29,22 +54,45 @@ function checkInputs() {
     setErrorFor(email, "Email is not valid");
   } else {
     setSuccessFor(email);
+    validEmail = true;
+    // localStorage.setItem("email", emailValue);
   }
 
   if (passwordValue === "") {
     setErrorFor(password, "Password cannot be blank");
+  } else if (passwordValue.length < 7) {
+    setErrorFor(password, "Password must be longer than 6 characters.");
+  } else if (!passwordValue.match(passw)) {
+    setErrorFor(password, "Password should contain at least one number.");
   } else {
     setSuccessFor(password);
+    validPwd = true;
+    // localStorage.setItem("password", passwordValue);
   }
 
   if (password2Value === "") {
     setErrorFor(password2, "Please confirm your password");
   } else if (passwordValue !== password2Value) {
     setErrorFor(password2, "Passwords does not match");
+  } else if (!password2Value.match(passw)) {
+    setErrorFor(password2, "Password should contain at least one number.");
+  } else if (password2Value.length < 7) {
+    setErrorFor(password2, "Password must be longer than 6 characters.");
   } else {
     setSuccessFor(password2);
+    validPwd2 = true;
   }
 }
+
+// function checkLogin() {
+//   const loginEmailValue = loginEmail.value.trim();
+
+//   if (loginEmailValue != localStorage.email) {
+//     setErrorFor(loginEmail, "No account with this email.");
+//   } else {
+//     setSuccessFor(loginEmail);
+//   }
+// }
 
 function setErrorFor(input, message) {
   const formControl = input.parentElement; // .form-control
